a7773cc931b63ad16d59898bfc01dccf9d28d8b1
Yaroslav <vizichkanich0709@gmail.com>
2023-10-02 21:22:31 +0300

    create user repo

---
+++ ThingsSales/ThingsSales.Data/Repositories/IRepository/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using ThingsSales.Model.Identity;
+
+namespace ThingsSales.Data.Repositories.IRepository
+{
+    public interface IUserRepository
+    {
+        Task<ApplicationUser> GetById(string userId);
+    }
+}
---
+++ ThingsSales/ThingsSales.Data/Repositories/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using ThingsSales.Data.ContextData;
+using ThingsSales.Data.Repositories.IRepository;
+using ThingsSales.Model.Identity;
+
+namespace ThingsSales.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ApplicationUser> GetById(string userId)
+        {
+            return await _context.ApplicationUsers.FindAsync(userId);
+        }
+    }
+}
