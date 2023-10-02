b54da677719decd71cb68a252aa592f79d6f7504
Yaroslav <vizichkanich0709@gmail.com>
2023-10-02 21:24:54 +0300

    create mapper

---
+++ ThingsSales/ThingsSales.Service/Mappings/UserMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ThingsSales.Model.Identity;
+using ThingsSales.Web.ViewModels;
+
+namespace ThingsSales.Service.Mappings
+{
+    public class UserMapper:Profile
+    {
+        public UserMapper()
+        {
+            CreateMap<AuthUserViewModel, ApplicationUser>();
+            CreateMap<ApplicationUser, AuthUserViewModel>();
+
+
+        }
+    }
+}
