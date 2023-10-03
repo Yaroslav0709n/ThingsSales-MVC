216929ff30ed0954a1b5721a79754792be353405
Yaroslav <vizichkanich0709@gmail.com>
2023-10-03 18:07:38 +0300

    new entity

---
+++ ThingsSales/ThingsSales.Model/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThingsSales.Model.Identity;
+
+namespace ThingsSales.Model
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public DateTime Time { get; set; }
+        public DateTime Date { get; set; }
+        public byte[] PhotoData { get; set; } 
+        public string PhotoMimeType { get; set; } 
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
