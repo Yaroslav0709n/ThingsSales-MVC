b9ac31d0c997ce1be2a4c08dce0c3e809aa77a2e
Yaroslav <vizichkanich0709@gmail.com>
2023-10-03 18:06:02 +0300

    check if null

---
+++ ThingsSales/ThingsSales.Data/ValidationExtensions/NullValidationExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace ThingsSales.Data.ValidationExtensions
+{
+    public static class NullValidationExtensions
+    {
+        public static T ThrowIfNull<T>(this T argument, string argumentName)
+        {
+            if (argument == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+
+            return argument;
+        }
+    }
+}
