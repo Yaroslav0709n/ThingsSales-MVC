dc6c7c97fd4667b9a5ccdd9fc515a8e0172b43a1
Yaroslav <vizichkanich0709@gmail.com>
2023-10-03 18:11:03 +0300

    new user item controller

---
+++ ThingsSales/ThingsSales.Web/Controllers/UserItemsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThingsSales.Web.Controllers
+{
+    public class UserItemsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
