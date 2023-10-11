d39d7eba82b9359816d058996f431182486db1b3
Yaroslav <vizichkanich0709@gmail.com>
2023-10-11 22:34:05 +0300

    new entity

---
+++ ThingsSales/ThingsSales.Model/Photo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThingsSales.Model
+{
+    public class Photo 
+    {
+        public int Id { get; set; }
+        public byte[] ImageData { get; set; }
+        public string Caption { get; set; }
+        
+        [ForeignKey("Item")]
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+    }
+}
