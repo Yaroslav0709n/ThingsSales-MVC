2492573052e8d3547d6d958abeffb346ae824664
Yaroslav <vizichkanich0709@gmail.com>
2023-10-11 22:27:02 +0300

    new prop in model

--- ThingsSales/ThingsSales.Service/ViewModels/ItemViewModel.cs
+++ ThingsSales/ThingsSales.Service/ViewModels/ItemViewModel.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using ThingsSales.Model;
 
 namespace ThingsSales.Service.ViewModels
 {
-    internal class ItemViewModel
+    public class ItemViewModel
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string City { get; set; }
+        public DateTime Time { get; set; }
+
+        [Required(ErrorMessage = "Please select at least one photo.")]
+        public ICollection<Photo> Photos { get; set; }
     }
 }
