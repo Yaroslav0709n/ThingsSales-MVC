c48f4800e5c4a0644ae6e1e9bd7b82b173efd957
Yaroslav <vizichkanich0709@gmail.com>
2023-10-11 22:36:14 +0300

    new mapper

---
+++ ThingsSales/ThingsSales.Service/Mappings/ItemMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ThingsSales.Model;
+using ThingsSales.Service.ViewModels;
+
+namespace ThingsSales.Service.Mappings
+{
+    public class ItemMapper:Profile
+    {
+        public ItemMapper()
+        {
+            CreateMap<ItemViewModel, Item>();
+            CreateMap<Item, ItemViewModel>();
+        }
+    }
+}
