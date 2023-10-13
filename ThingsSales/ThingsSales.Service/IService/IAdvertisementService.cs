4ae2b33fc58f50fe8b0a9e24d73c1e93d1befdfd
Yaroslav <vizichkanich0709@gmail.com>
2023-10-14 00:10:34 +0300

    renamed

---
+++ ThingsSales/ThingsSales.Service/IService/IAdvertisementService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using ThingsSales.Model;
+using ThingsSales.Service.ViewModels;
+
+namespace ThingsSales.Service.IService
+{
+    public interface IAdvertismentService
+    {
+        Task<AdvertisementViewModel> InsertItem(AdvertisementViewModel item, List<IFormFile> photos, string userId);
+        Task<IEnumerable<AdvertisementViewModel>> GetAllItems(string userId);
+        Task<AdvertisementViewModel> GetItemById(int itemId);
+    }
+}
