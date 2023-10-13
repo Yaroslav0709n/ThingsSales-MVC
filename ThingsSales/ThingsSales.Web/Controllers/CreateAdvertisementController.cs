2bb36bd5f0ccefb2e91d63f1c656db099d96bbcc
Yaroslav <vizichkanich0709@gmail.com>
2023-10-14 00:12:35 +0300

    renamed

---
+++ ThingsSales/ThingsSales.Web/Controllers/CreateAdvertisementController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThingsSales.Service.IService;
+using ThingsSales.Service.ViewModels;
+
+namespace ThingsSales.Web.Controllers
+{
+    public class CreateAdvertisementController : Controller
+    {
+        private readonly IAdvertismentService _advertismentService;
+        public CreateAdvertisementController(IAdvertismentService advertismentService)
+        {
+            _advertismentService = advertismentService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult> InsertItem(AdvertisementViewModel item, List<IFormFile> photos)
+        {
+            string userId = HttpContext.Session.GetString("UserId");
+
+            await _advertismentService.InsertItem(item, photos, userId);
+
+            return RedirectToAction("Index", "UserAdvertisements");
+        }
+    }
+}
